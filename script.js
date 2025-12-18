3781a5ea7f2016c1f6cf5ca9f2234b014fb1b1a6
Maximilian Hepach <hepach@posteo.de>
2025-12-18 14:15:28 +0000

    Use local QR code library instead of CDN for SourceHut compatibility

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function setupPosterButton() {
   <head>
     <meta charset="UTF-8" />
     <title>Weather, Climate &amp; Health – Events Poster</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+    <script src="qrcode.min.js"></script>
     <style>
       :root {
         --text: #111827;
